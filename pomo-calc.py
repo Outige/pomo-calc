5f02073b54e4c3f85e1404756c71dcea3a61bc96
Tieg <tiegosullivanpsnl@gmail.com>
2020-04-27 13:01:25 +0200

    added current time calculation + file.txt ofset system

--- pomo-calc.py
+++ pomo-calc.py
@@ -1,22 +1,48 @@
 from datetime import datetime
 import sys
 from math import floor
+from random import randrange as rr
+
+PLEN = 25
+OFF = 3
+
+def test_file():
+    try:
+        fp = open('file.txt', 'r')
+        fp.close()
+    except IOError:
+        return 0
+    return 1
+
+def save_state():
+    if test_file():
+        return
+    fp = open('file.txt', 'w')
+    fp.write(str(rr(PLEN-OFF, PLEN+OFF+1)))
+    fp.close()
+
+def load_state():
+    for line in open('file.txt'):
+        line = int(line)
+    return line
 
 if __name__ == "__main__":
+    save_state()
+    oset = load_state()
     a = input("Start time(hh-mm):")
-    b = input("Start time(hh-mm):")
+    b = str(datetime.now().hour) + ":" + str(datetime.now().minute) #input("Start time(hh-mm):")
     a = datetime.strptime(a, "%H:%M")
     b = datetime.strptime(b, "%H:%M")
 
     hours = int(b.hour-a.hour)
     mins = int(b.minute-a.minute) + hours*60
     
-    x = floor(mins/25)
-    m = mins - x*25
+    x = floor(mins/oset)
+    m = mins - x*oset
     if x > 1:
         print("X" + str(x) + " + " + str(m))
     elif x == 1:
         print("+" + str(m))
     elif x == 0:
-        m = 25 - m
+        m = oset - m
         print("-" + str(m))
