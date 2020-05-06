8da613e710db0177be9a6afab6bbb250a6fbc6a8
Tieg <tiegosullivanpsnl@gmail.com>
2020-05-06 12:19:30 +0200

    temp state, adding grading

--- pomo-calc.py
+++ pomo-calc.py
@@ -4,8 +4,17 @@ from math import floor
 from random import randrange as rr
 import os
 
+# ----------------------------------
+# globals
+# ----------------------------------
+
 PLEN = 25
 OFF = 3
+
+
+# ----------------------------------
+# files
+# ----------------------------------
 
 def test_file():
     try:
@@ -26,12 +35,7 @@ def delete_state():
         return
     os.remove('file.txt')
     print('state deleted')
-    
-    # if test_file():
-    #     return
-    # fp = open('file.txt', 'w')
-    # fp.write(str(rr(PLEN-OFF, PLEN+OFF+1)))
-    # fp.close()
+
 
 def save_state():
     if test_file():
@@ -45,6 +49,13 @@ def load_state():
         line = int(line)
     return line
 
+def append_record(time, grade, date):
+    1
+
+# ----------------------------------
+# helper
+# ----------------------------------
+
 def handle_input():
     if len(sys.argv) >= 2:
         a = sys.argv[1]
@@ -53,20 +64,20 @@ def handle_input():
     a = datetime.strptime(a, "%H:%M")
     return a
 
-if __name__ == "__main__":
+
+# ----------------------------------
+# systems
+# ----------------------------------
+
+def relative_system():
     delete_state()
     save_state()
     oset = load_state()
-    # a = input("Start time(hh-mm):")
-    # b = str(datetime.now().hour) + ":" + str(datetime.now().minute) #input("Start time(hh-mm):")
-    # a = datetime.strptime(a, "%H:%M")
-    # b = datetime.strptime(b, "%H:%M")
     a = handle_input()
     b = datetime.now()
 
     hours = int(b.hour-a.hour)
     mins = int(b.minute-a.minute) + hours*60
-    
     x = floor(mins/oset)
     m = mins - x*oset
     if x > 1:
@@ -76,3 +87,26 @@ if __name__ == "__main__":
     elif x == 0:
         m = oset - m
         print("-" + str(m))
+
+def grade_system():
+    a = handle_input()
+    b = datetime.now()
+
+    hours = int(b.hour-a.hour)
+    mins = int(b.minute-a.minute) + hours*60
+    oset = 25
+    if mins < oset:
+        print('F')
+    elif mins >= oset and mins < oset*1.5:
+        print('C')
+    elif mins >= oset*1.5 and mins < oset*2:
+        print('B')
+    elif mins >= oset*2:
+        print('A')
+    print(mins)
+    return
+
+
+if __name__ == "__main__":
+    relative_system()
+    # grade_system()
